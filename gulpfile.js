8748f674fe4b69cd97ee65963066b00ca575b31b
Olzhas Kaliyev <Olzhas.Kaliyev.90@Gmail.com>
2020-04-22 17:30:44 +0600

    gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,143 +1,124 @@
-const {src, dest, watch, series, parallel, lastRun} = require('gulp');
+const { src, dest, watch, series, parallel, lastRun } = require('gulp');
 const $ = require('gulp-load-plugins')();
 const del = require('del');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
+const port = require('yargs').argv.port || 9000;
+const dev = process.env.NODE_ENV === 'development';
 
-let isDev = process.env.NODE_ENV === 'development';
-
-const develop = series(
-  clean,
-  parallel(templatesCached, styles, scripts),
-  function serveDevelopment() {
-    browserSync.init({
-      notify: false,
-      port: 9000,
-      server: {
-        baseDir: ['.tmp', 'src'],
-        routes: {'/node_modules': 'node_modules'}
-      }
-    });
-    watch('src/*.pug', templatesCached);
-    watch('src/templates/**/*.pug', templates);
-    watch('src/styles/**/*.scss', styles);
-    watch('src/scripts/**/*.js', scripts);
-    watch(['src/*.html', 'src/images/**/*', 'src/fonts/**/*'])
-      .on('change', browserSync.reload);
-  }
-);
-
-const build = series(
-  clean,
-  parallel(
-    series(
-      parallel(html, templates, styles, scripts),
-      concat),
-    images,
-    fonts,
-    extras
-  )
-);
-
-const serveDist = series(
-  build,
-  function serveProduction() {
-    browserSync.init({
-      notify: false,
-      port: 9000,
-      server: {
-        baseDir: 'dist',
-        routes: {'/node_modules': 'node_modules'}
-      }
-    });
-  }
-);
-
-function concat() {
-  return src('.tmp/*.html')
-    .pipe($.useref({searchPath: ['.tmp', '.']}))
-    .pipe($.if(/\.css$/, $.postcss([cssnano({safe: true, autoprefixer: false})])))
-    .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
-    // .pipe($.if(/\.css$/, dest('dist')))
-    // .pipe($.if(/\.js$/, dest('dist')))
-    .pipe(dest('dist'));
-}
-
-function html() {
-  return src('src/*.html')
-    .pipe(dest('.tmp'));
+// Tasks
+function clean() {
+  return del(['.tmp', 'dist'])
 }
 
 function templates() {
   return src('src/*.pug')
     .pipe($.plumber())
-    .pipe($.pug({pretty: true}))
+    .pipe($.pug({ pretty: true }))
     .pipe(dest('.tmp'));
 }
 
 function templatesCached() {
-  return src('src/*.pug', {since: lastRun(templatesCached)})
+  return src('src/*.pug', { since: lastRun(templatesCached) })
     .pipe($.plumber())
-    .pipe($.pug({pretty: true}))
+    .pipe($.pug({ pretty: true }))
     .pipe(dest('.tmp'))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function styles() {
-  return src('src/styles/*.scss')
+  return src('src/styles/*.scss', { sourcemaps: dev })
     .pipe($.plumber())
-    .pipe($.if(isDev, $.sourcemaps.init()))
     .pipe($.sass.sync({
       outputStyle: 'expanded',
       precision: 10,
-      includePaths: ['.']
+      includePaths: ['.'],
     }).on('error', $.sass.logError))
     .pipe($.postcss([autoprefixer()]))
-    .pipe($.if(isDev, $.sourcemaps.write()))
-    .pipe(dest('.tmp/styles'))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(dest('.tmp/styles', { sourcemaps: dev }))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function scripts() {
-  return src('src/scripts/**/*.js')
+  return src('src/scripts/**/*.js', { sourcemaps: dev })
     .pipe($.plumber())
-    .pipe($.if(isDev, $.sourcemaps.init()))
-    .pipe($.if('main.js', $.babel()))
-    .pipe($.if(isDev, $.sourcemaps.write('.')))
-    .pipe(dest('.tmp/scripts'))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe($.babel())
+    .pipe(dest('.tmp/scripts', { sourcemaps: dev ? '.' : false }))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function minify() {
+  return src(['src/*.html', '.tmp/*.html'])
+    .pipe($.useref({ searchPath: ['.tmp', 'src', '.'] }))
+    .pipe($.if(/\.css$/, $.postcss([cssnano({ safe: true, autoprefixer: false })])))
+    .pipe($.if(/\.js$/, $.uglify({ compress: { drop_console: true } })))
+    .pipe(dest('dist'));
 }
 
 function images() {
-  return src('src/images/**/*', {since: lastRun(images)})
+  return src('src/images/**/*', { since: lastRun(images) })
     .pipe($.imagemin())
     .pipe(dest('dist/images'));
 }
 
 function fonts() {
   return src('src/fonts/**/*')
-    .pipe($.if(isDev, dest('.tmp/fonts'), dest('dist/fonts')));
+    .pipe($.if(dev, dest('.tmp/fonts'), dest('dist/fonts')));
 }
 
-function extras() {
-  return src(['src/*', '!src/*.html', '!src/*.pug'], {dot: true})
+function files() {
+  return src(['src/*', '!src/*.html', '!src/*.pug'], { dot: true })
     .pipe(dest('dist'));
 }
 
-function clean() {
-  return del(['.tmp', 'dist'])
+// Server
+function serve() {
+  browserSync.init({
+    notify: false,
+    port,
+    server: {
+      baseDir: dev ? ['.tmp', 'src'] : 'dist',
+      routes: { '/node_modules': 'node_modules' },
+    },
+  });
+
+  if (dev) {
+    watch('src/*.pug', templatesCached);
+    watch('src/templates/**/*.pug', templates)
+      .on('change', browserSync.reload);
+    watch('src/styles/**/*.scss', styles);
+    watch('src/scripts/**/*.js', scripts);
+    watch('src/fonts/**/*', fonts);
+    watch(['src/*.html', 'src/images/**/*', '.tmp/fonts/**/*'])
+      .on('change', browserSync.reload);
+  }
 }
 
-exports.default = develop;
-exports.build = build;
-exports.serveDist = serveDist;
-exports.concat = series(parallel(html, templates, styles, scripts), concat);
-exports.html = html;
+// Export tasks
+exports.default = series(
+  clean,
+  parallel(templates, styles, scripts),
+  serve,
+);
+
+exports.build = series(
+  clean,
+  parallel(
+    series(
+      parallel(templates, styles, scripts),
+      minify,
+    ),
+    images, fonts, files,
+  ),
+  serve,
+);
+
+exports.clean = clean;
 exports.templates = templates;
 exports.styles = styles;
 exports.scripts = scripts;
+exports.minify = minify;
 exports.images = images;
 exports.fonts = fonts;
-exports.extras = extras;
-exports.clean = clean;
+exports.files = files;
